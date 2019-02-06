3d93743d09db8ea8ab00d15283738d3f8d632119
MatthewMeyers <jedimastermeyers@gmail.com>
2019-02-05 19:01:41 -0700

    began working on ai

---
+++ Aztecha/Assets/Scripts/AnimationBehaviours/FollowBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowBehaviour : StateMachineBehaviour
+{
+   public GameObject player;
+   public NavMeshAgent ai;
+
+   override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+   {
+      player = GameObject.FindGameObjectWithTag("Player");
+      ai = animator.gameObject.GetComponent<NavMeshAgent>();
+   }
+
+   override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+   {
+      ai.SetDestination(player.transform.position);
+      ai.stoppingDistance = 5;
+   }
+
+   override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+   {
+      
+   }
+}
--- Aztecha/Assets/Scripts/SquadController.cs
+++ Aztecha/Assets/Scripts/SquadController.cs
@@ -7,17 +7,31 @@ using UnityEngine.AI;
 public class SquadController : MonoBehaviour
 {
     private NavMeshAgent ai;
+    private Animator anim;
 
     private void Start()
     {
         ai = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetBool("Order", false);
+            anim.SetBool("FollowPlayer", true);
+        }
     }
 
     public void PerformOrder(string _tag, Vector3 _loc)
     {
         if(_tag == "order")
         {
+            anim.SetBool("FollowPlayer", false);
+            anim.SetBool("Order", true);
             ai.SetDestination(_loc);
+            ai.stoppingDistance = 0;
         }
     }
 }
