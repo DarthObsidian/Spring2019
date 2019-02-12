f93974cd397b6066b545e178152fe4d4d32497a9
MatthewMeyers <jedimastermeyers@gmail.com>
2019-02-11 22:22:26 -0700

    added power core to scene and messed with lighting

---
+++ LevelDesign/SPACESHIP/Assets/Scripts/PowerCoreController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerCoreController : MonoBehaviour
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        StartCoroutine(Play());    
+    }
+
+    IEnumerator Play()
+    {
+        while(true)
+        {
+            int num = Random.Range(0,10);
+            if(num > 6)
+            {
+                anim.SetBool("break", true);
+            }
+            yield return new WaitForSeconds(3f);
+            anim.SetBool("break", false);
+        }  
+    }
+}
