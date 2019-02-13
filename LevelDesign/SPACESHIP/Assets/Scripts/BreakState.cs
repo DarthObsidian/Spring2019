02c5b48654001bde42ddc9a9d437777fb3995164
Matthew Meyers <jedimastermeyers@gmail.com>
2019-02-13 12:44:39 -0700

    UV mapped Power Stands and updated project version

---
+++ LevelDesign/SPACESHIP/Assets/Scripts/BreakState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class BreakState : StateMachineBehaviour
+{
+    public VisualEffect vfx;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       animator.SetBool("break", false);
+       GameObject game = GameObject.FindWithTag("PowerSource");
+       vfx = game.GetComponent<VisualEffect>();
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       vfx.SetVector2("MinMax", new Vector2(0,1));
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
--- LevelDesign/SPACESHIP/Assets/Scripts/PowerCoreController.cs
+++ LevelDesign/SPACESHIP/Assets/Scripts/PowerCoreController.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.VFX;
 
 public class PowerCoreController : MonoBehaviour
 {
     private Animator anim;
+    public VisualEffect vfx;
+
+    private int coolDown = 2;
 
     void Start()
     {
@@ -17,12 +21,14 @@ public class PowerCoreController : MonoBehaviour
         while(true)
         {
             int num = Random.Range(0,10);
-            if(num > 6)
+            if(num > 6 && coolDown < 1)
             {
                 anim.SetBool("break", true);
+                vfx.SetVector2("MinMax", new Vector2(1,5));
+                coolDown = 10;
             }
             yield return new WaitForSeconds(3f);
-            anim.SetBool("break", false);
+            coolDown--;
         }  
     }
 }
