36026cc6344c6a5bccd44436799708bfdbd9530a
Matthew Meyers <jedimastermeyers@gmail.com>
2019-02-27 13:18:28 -0700

    Edit fire and lighting

---
+++ LevelDesign/SPACESHIP/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    Light lt;
+    public float minIntensity = 0.25f;
+    public float maxIntensity = 0.5f;
+ 
+    float random;
+ 
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        random = Random.Range(0.0f, 65535.0f);
+    }
+ 
+    void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time);
+        lt.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+    }
+ 
+}
