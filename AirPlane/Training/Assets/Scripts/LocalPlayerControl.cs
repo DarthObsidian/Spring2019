8e81bd38eda0f1f53274f7bf1802ec811e50086e
MatthewMeyers <jedimastermeyers@gmail.com>
2019-03-21 13:37:48 -0600

    worked on networking

---
+++ AirPlane/Training/Assets/Scripts/LocalPlayerControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LocalPlayerControl : NetworkBehaviour
+{
+    public GameObject camRig;
+    public Transform leftHand;
+    public Transform rightHand;
+    public Camera leftEye;
+    public Camera rightEye;
+    Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+}
