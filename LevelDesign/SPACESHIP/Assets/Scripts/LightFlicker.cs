0db78d04ee1aa0204cea82037149417bd282d1e0
Matthew Meyers <jedimastermeyers@gmail.com>
2019-03-27 10:35:04 -0600

    Improved power stands model, fire flicker, and fire timing

--- LevelDesign/SPACESHIP/Assets/Scripts/LightFlicker.cs
+++ LevelDesign/SPACESHIP/Assets/Scripts/LightFlicker.cs
@@ -7,6 +7,7 @@ public class LightFlicker : MonoBehaviour
     Light lt;
     public float minIntensity = 0.25f;
     public float maxIntensity = 0.5f;
+    public float speed;
  
     float random;
  
@@ -18,7 +19,7 @@ public class LightFlicker : MonoBehaviour
  
     void Update()
     {
-        float noise = Mathf.PerlinNoise(random, Time.time);
+        float noise = Mathf.PerlinNoise(random, Time.time * speed);
         lt.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
     }
  
