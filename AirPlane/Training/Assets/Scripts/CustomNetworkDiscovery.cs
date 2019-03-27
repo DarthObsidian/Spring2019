838619bc942f8bf85f2bc0836a506a3b66349be1
Matthew Meyers <jedimastermeyers@gmail.com>
2019-03-27 12:40:00 -0600

    improved custom network

--- AirPlane/Training/Assets/Scripts/CustomNetworkDiscovery.cs
+++ AirPlane/Training/Assets/Scripts/CustomNetworkDiscovery.cs
@@ -5,24 +5,26 @@ using UnityEngine.Networking;
 
 public class CustomNetworkDiscovery : NetworkDiscovery
 {
-    List<string> addresses;
+    List<string> addresses = new List<string>();
     bool exists;
 
     public override void OnReceivedBroadcast(string fromAddress, string data)
     {
         foreach (string address in addresses)
         {
-            if(fromAddress == address)
+            if (fromAddress == address)
             {
                 exists = true;
             }
         }
-        if(!exists)
+
+        if (!exists)
         {
             addresses.Add(fromAddress);
             exists = false;
             string add = fromAddress;
-            add.Replace("::ffff:", "");
+            add = add.Replace("::ffff:","");
+            print(add);
             NetworkManager.singleton.networkAddress = add;
         }
     }
