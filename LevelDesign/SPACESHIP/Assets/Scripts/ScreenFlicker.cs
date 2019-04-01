5ecf452c69ce41cedf1d9502cf877a3156858f42
Matthew Meyers <jedimastermeyers@gmail.com>
2019-04-01 10:52:22 -0600

    Edited console screen shader

---
+++ LevelDesign/SPACESHIP/Assets/Scripts/ScreenFlicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFlicker : MonoBehaviour
+{
+    Renderer cs;
+    Coroutine myCor;
+    float timeScale = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cs = GetComponent<Renderer>();
+        myCor = StartCoroutine(Flicker());
+    }
+
+    IEnumerator Flicker()
+    {
+        while(true)
+        {
+            cs.material.SetFloat("_Magnitude", Random.Range(0f, 0.2f));
+            yield return new WaitForSeconds(timeScale);
+        }
+    }
+}
