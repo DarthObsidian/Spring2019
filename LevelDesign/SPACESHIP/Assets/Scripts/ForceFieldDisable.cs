77598b6c35d2a89a47c82419d6061dd8900d3544
Matthew Meyers <jedimastermeyers@gmail.com>
2019-04-08 10:36:49 -0600

    Added head bobbing, worked on audio

---
+++ LevelDesign/SPACESHIP/Assets/Scripts/ForceFieldDisable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceFieldDisable : Interact
+{
+    AudioSource aud;
+    public AudioSource shieldAud;
+    public List<GameObject> objs;
+    bool used;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+
+    public override void doStuff()
+    {
+        if(!used)
+        {
+            aud.Play();
+            StartCoroutine(WaitForAudio());
+            used = true;
+        }
+        
+    }
+
+    IEnumerator WaitForAudio()
+    {
+        while(aud.isPlaying)
+        {
+            yield return new WaitForSeconds(0.01f);
+        }
+        shieldAud.Play();
+
+        while(shieldAud.isPlaying)
+        {
+            yield return new WaitForSeconds(0.01f);
+        }
+        
+        foreach (GameObject item in objs)
+        {
+            item.SetActive(false);
+        }
+    }
+}
---
+++ LevelDesign/SPACESHIP/Assets/Scripts/RandomNoise.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNoise : MonoBehaviour
+{
+    public List<AudioClip> clips;
+    AudioSource aud;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+        StartCoroutine(RandomTime());
+    }
+
+    IEnumerator RandomTime()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(10f);
+            int noise = Random.Range(0, clips.Capacity);
+
+            aud.clip = clips[noise];
+            aud.Play();
+        }
+    }
+}
